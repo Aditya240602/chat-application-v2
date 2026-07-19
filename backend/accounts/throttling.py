6e8ef38deabad8838d47ccaa78cc87b79c9b2801
Aditya Kumar <adityasarraff24082006@gmail.com>
2026-07-19 13:24:45 +0530

    s

--- backend/accounts/throttling.py
+++ backend/accounts/throttling.py
@@ -1,13 +1,13 @@
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 class LoginThrottle(AnonRateThrottle):
-    rate = '5/min'
+    rate = "10/min"
 
 class RegisterThrottle(AnonRateThrottle):
-    rate = '3/min'
+    rate = "3/min"
 
 class SendMessageThrottle(UserRateThrottle):
-    rate = '20/min'
+    rate = "120/min"
 
     def allow_request(self, request, view):
         # Only throttle POST requests (sending messages), allow GET (polling/fetching)
